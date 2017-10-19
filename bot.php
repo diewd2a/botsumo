996db91e7839678cf511989c80393365b370edd7
diewd2a <diewd2a@gmail.com>
2017-10-20 01:51:47 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -26,23 +26,23 @@ if (!is_null($events['events'])) {
 				//$text = $codem[0].' =>  '.$codem[1];	
 
  				$access_token = 'ftnj1K6SBDQnLoJbTHwFaKoMooMOS7Ax7zCRIV4Rfm6I3z6Qds';
-				$url = '210.1.58.130/~demomlm/app/v1.0/index.php/member/dashboard/';
-				
-				$headers = array('Authorization: Bearer ' . $access_token);
-				
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				curl_setopt($ch, CURLOPT_POST, 1);
-				curl_setopt($ch, CURLOPT_POSTFIELDS,
-							"mem_id=0000001");
-				$result = curl_exec($ch);
-				curl_close($ch);
-				
-				//echo $result;
-				$jsonde = json_decode($result, true, 512, JSON_UNESCAPED_UNICODE);
-				$text = 'รหัสสมาชิก	 = '.$jsonde[0]['data_profile']['mem_id'].'
+$url = '210.1.58.130/~demomlm/app/v1.0/index.php/member/dashboard/';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "mem_id=0000001");
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+$jsonde = json_decode($result, true, 512, JSON_UNESCAPED_UNICODE);
+$text = 'รหัสสมาชิก	 = '.$jsonde[0]['data_profile']['mem_id'].'
 				';
 				$text .= 'ประเภทสมาชิก		 = '.$jsonde[0]['data_profile']['type'].'
 				';
@@ -50,8 +50,9 @@ if (!is_null($events['events'])) {
 				';
 				$text .= 'คะแนนส่วนตัว		 = '.$jsonde[0]['data_pv']['per_score']['per_pv'].'
 				';
-				$text .= 'รายละเอียด	 = '.$jsonde[0]['data_pv']['per_score']['link_detail'].'
+				$text .= 'รายละเอียด	 = '.$jsonde[0]['data_pv']['per_score']['link_detail'].'0000001'.'
 				';
+//echo $text;	
 				
 			}
 			else{
